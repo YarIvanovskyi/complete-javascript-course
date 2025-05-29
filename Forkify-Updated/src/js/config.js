887fb49d4b1c7c4825f183d86786862536ef1c3a
Yaroslav Ivanovskyi <ivanovskyi.ya@outlook.com>
2025-05-29 19:53:14 +0300

    U

---
+++ Forkify-Updated/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6bbe7d59-0bfd-4f7e-96c5-2a103fd3648a';
+export const MODAL_CLOSE_SEC = 2.5;
